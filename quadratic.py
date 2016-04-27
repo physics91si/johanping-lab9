3433256a941ae3c9765fae68036db4f009f7214d
Hannah O'Day <odayj@DN0a233a04.SUNet>
2016-04-26 18:27:16 -0700

    finished my lab

--- quadratic.py
+++ quadratic.py
@@ -6,21 +6,30 @@ import sys
 #Enter values for a, b, and c in the command line
 #e.g. run: >python quadratic.py 1 2 -15
 def main():
-    a = sys.argv[1]
-    b = sys.argv[2]
-    c = sys.argv[3]
-    x1, x2 = find_roots(a, b, c)
-    print ("This is x1: %f" %x1)
-    print ("This is x2: %f" %x2)
+    try: 
+        a = int(sys.argv[1])
+        if a == 0:
+        	raise ValueError("Please type an integer value for a")
+
+        b = int(sys.argv[2])
+        c = int(sys.argv[3])
+        x1, x2 = find_roots(a, b, c)
+        print ("This is x1: %f" %x1)
+        print ("This is x2: %f" %x2)
+    except ValueError:
+        print("That is not a valid entry, please enter an integer")
+        
+        
 
 
-def find_roots(a,b,c):
-    mid = b^2 - 4*a*c
-    sqrt_mid = mid^(1/2)
-    x1 = (-b + sqrt_mid)/2*a
-    x2 = (-b - sqrt_mid)/2*a
+def find_roots(a,b,c):   
+    mid = b**2 - 4*a*c
+    if mid < 0:
+        raise ValueError("This function has imaginary roots")
+    sqrt_mid = mid**(1/2)
+    x1 = (-b + sqrt_mid)/(2*a)
+    x2 = (-b - sqrt_mid)/(2*a)
     return x1, x2
-
 
 if __name__=="__main__":
         main()
